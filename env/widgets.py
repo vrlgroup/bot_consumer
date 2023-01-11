a592402eff617aee8e4b2a64bf805ddc920921fa
Carlos Amaral <carlosabdoamaral@gmail.com>
2023-01-11 20:08:12 -0300

    adding env folder

---
+++ env/widgets.py
@@ -0,0 +1,15 @@
+searchbarXpath = "/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/button/div[2]/span"
+searchbarInputXpath = "/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]"
+
+chatInputXpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]"
+chatSubmitMessageBtnXpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button"
+
+groupSettingsXpath = "/html/body/div[1]/div/div/div[4]/div/header/div[3]/div/div[2]/div/div"
+groupSelectMessagesXpath = "/html/body/div[1]/div/span[4]/div/ul/div/div/li[2]"
+
+# lastMessageCheckbox = "/html/body/div[1]/div/div/div[4]/div/div[2]/div/div[2]/div[2]/div[6]/div/span/div/div"
+firstMessageCheckboxXpath = "/html/body/div[1]/div/div/div[4]/div/div[2]/div/div[2]/div[2]/div[4]/div/span/div/input"
+
+forwardMessageSpanXpath = "/html/body/div[1]/div/div/div[4]/div/span[2]/div/button[4]/span"
+forwardSelectFirstGroupXpath = "/html/body/div[1]/div/span[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div[2]"
+forwardMessageSubmitXpath = "/html/body/div[1]/div/span[2]/div/div/div/div/div/div/div/span/div/div/div"
